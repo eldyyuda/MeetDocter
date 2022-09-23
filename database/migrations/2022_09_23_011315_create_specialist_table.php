efc36cd39d75f7ee6a17e31d17431967316ac542
eldyyuda <eldyyudakurniawan@gmail.com>
2022-09-23 09:34:11 +0700

    add migration

--- database/migrations/2022_09_23_011315_create_specialist_table.php
+++ database/migrations/2022_09_23_011315_create_specialist_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateSpecialistTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('specialist', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('price');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -34,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('specialist');
     }
-};
+}
