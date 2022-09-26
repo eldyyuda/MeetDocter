32216a9beb02632b72d6f7c838cb94c445a73b3b
eldyyuda <eldyyudakurniawan@gmail.com>
2022-09-26 20:00:41 +0700

    add model

---
+++ app/Models/ManagementAccess/RoleUser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models\ManagementAccess;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class RoleUser extends Model
+{
+    // use HasFactory;
+    use SoftDeletes;
+    public $table = 'role_user';
+
+    protected $dates = [
+        'created_at',
+        'update_at',
+        'delete_at'
+    ];
+
+    protected $fillable = [
+        'user_id',
+        'role_id',
+        'created_at',
+        'updated_at',
+        'delete_at'
+    ];
+}
