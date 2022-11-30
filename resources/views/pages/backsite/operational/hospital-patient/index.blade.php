6c0206c5b565cc47e65e93616aa59109a383ef18
eldyyuda <eldyyudakurniawan@gmail.com>
2022-11-30 13:57:35 +0700

    rename migration foreign-key and make index patient

---
+++ resources/views/pages/backsite/operational/hospital-patient/index.blade.php
@@ -0,0 +1,126 @@
+@extends('layouts.app')
+
+{{-- set title --}}
+@section('title', 'Hospital Patient')
+
+@section('content')
+
+<!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="content-wrapper">
+
+            {{-- error --}}
+            @if ($errors->any())
+                <div class="alert bg-danger alert-dismissible mb-2" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            {{-- breadcumb --}}
+            <div class="content-header row">
+                <div class="content-header-left col-md-6 col-12 mb-2 breadcrumb-new">
+                    <h3 class="content-header-title mb-0 d-inline-block">Hospital Patient</h3>
+                    <div class="row breadcrumbs-top d-inline-block">
+                        <div class="breadcrumb-wrapper col-12">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="{{ route('backsite.dashboard.index') }}">Dashboard</a></li>
+                                <li class="breadcrumb-item active">Hospital Patient</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {{-- table card --}}
+            @can('hospital_patient_table')
+                <div class="content-body">
+                    <section id="table-home">
+                        <!-- Zero configuration table -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h4 class="card-title">Hospital Patient List</h4>
+                                        <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                                        <div class="heading-elements">
+                                            <ul class="list-inline mb-0">
+                                                <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                                <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                                <!-- <li><a data-action="close"><i class="ft-x"></i></a></li> -->
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    <div class="card-content collapse show">
+                                        <div class="card-body card-dashboard">
+
+                                            <div class="table-responsive">
+                                                <table class="table table-striped table-bordered text-inputs-searching default-table">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Name</th>
+                                                            <th>Type User</th>
+                                                            <th>Kontak</th>
+                                                            <th>Alamat</th>
+                                                            <th>Umur</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        @forelse($hospitalPatient as $key => $hospitalPatient_item)
+                                                            <tr data-entry-id="{{ $hospitalPatient_item->id }}">
+                                                                <td>{{ $hospitalPatient_item->user->name ?? '' }}</td>
+                                                                <td>{{ $hospitalPatient_item->typeUser->name ?? '' }}</td>
+                                                                <td>{{ $hospitalPatient_item->contact ?? '' }}</td>
+                                                                <td>{{ $hospitalPatient_item->address ?? '' }}</td>
+                                                                <td>{{ $hospitalPatient_item->age ?? '' }}</td>
+                                                            </tr>
+                                                        @empty
+                                                            {{-- not found --}}
+                                                        @endforelse
+                                                    </tbody>
+                                                    <tfoot>
+                                                        <tr>
+                                                            <th>Name</th>
+                                                            <th>Type User</th>
+                                                            <th>Kontak</th>
+                                                            <th>Alamat</th>
+                                                            <th>Umur</th>
+                                                        </tr>
+                                                    </tfoot>
+                                                </table>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            @endcan
+
+        </div>
+    </div>
+<!-- END: Content-->
+
+@endsection
+
+@push('after-script')
+    <script>
+        $('.default-table').DataTable( {
+            "order": [],
+            "paging": true,
+            "lengthMenu": [ [5, 10, 25, 50, 100, -1], [5, 10, 25, 50, 100, "All"] ],
+            "pageLength": 10
+        });
+    </script>
+@endpush
